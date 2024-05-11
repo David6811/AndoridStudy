2dde50001cf346881f68429a45875e516afcb157
David <86584824+David6811@users.noreply.github.com>
2024-05-11 19:24:08 +1000

    Implement basic button and onclick event (#4)

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.androidstudy
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button1)
+        button.setOnClickListener {
+            // Implement your onClick logic here
+            button.text = getString(R.string.i_am_changed)
+        }
+
     } }

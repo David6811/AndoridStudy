5a650535c455d5bc67d856b0eabdedc729df2046
weixu <xuwei19850423@gmail.com>
2024-05-11 18:58:05 +1000

    Implements static fragment

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -2,31 +2,10 @@ package com.example.androidstudy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.example.androidstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-
-        binding.fragment1Btn.setOnClickListener{
-            replaceFragment(Fragment1())
-        }
-
-        binding.fragment2Btn.setOnClickListener{
-            replaceFragment(Fragment2())
-        }
-    }
-
-    private fun replaceFragment(fragment : Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
-        fragmentTransaction.commit()
-    }
-}
+        setContentView(R.layout.activity_main)
+    } }

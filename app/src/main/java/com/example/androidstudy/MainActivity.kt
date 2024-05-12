455c7be4f09fe376be006704fe70bc8dd59e58fb
David <86584824+David6811@users.noreply.github.com>
2024-05-12 17:14:45 +1000

    View binding (#8)
    * recover to init
    
    * change the app name to student information
    
    * add a ImageView for anycopy logo
    
    * completed information collection layout
    
    * ViewBinding
    
    * collect information

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -1,61 +1,53 @@
 package com.example.androidstudy
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidstudy.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
-    private lateinit var imageView: ImageView
-    private lateinit var textView: TextView
-    private lateinit var editText: EditText
-    private var flag = 1;
-
-    @SuppressLint("InflateParams")
+    private lateinit var binding: ActivityMainBinding
+    private var info: String = ""
+    private var selected = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        imageView = findViewById(R.id.imageView1)
+        val checkBoxListener = CheckBoxListener()
+        binding.cbJava.setOnCheckedChangeListener(checkBoxListener)
+        binding.cbAndroid.setOnCheckedChangeListener(checkBoxListener)
+        binding.cbEnglish.setOnCheckedChangeListener(checkBoxListener)
 
-        textView = findViewById(R.id.textView2)
-        editText = findViewById(R.id.editText1)
 
-        button = findViewById(R.id.button1)
-        button.setOnClickListener {
-            button.text = getString(R.string.i_am_changed)
-            textView.text = editText.getText().toString()
+        binding.btConfirmation.setOnClickListener {
+            val etName = binding.etName.text.toString()
+            val etPhone = binding.etPhone.text.toString()
+            val rgid = binding.rgGender.checkedRadioButtonId
+            val gender = if (rgid == R.id.rb_male) "Male" else "Female"
+            val info = "Name: $etName, mobile: $etPhone, gender: $gender, favorite: $selected"
+            val sb = Snackbar.make(binding.conLayout, info, BaseTransientBottomBar.LENGTH_LONG)
+            sb.setAction("Confirm") {
+                Toast.makeText(this@MainActivity, "Confirmed", Toast.LENGTH_SHORT).show()
+            }
+            sb.show()
+        }
+    }
 
-            //Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_LONG).show()
-//            Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_LONG)
-//                .apply { setGravity(Gravity.CENTER, 0, 0) }
-//                .show()
-
-            // Inflate the custom layout for the toast message
-            val inflater = LayoutInflater.from(this@MainActivity)
-            val layout = inflater.inflate(R.layout.toast_layout, null)
-
-            val toast = Toast(this@MainActivity)
-            toast.duration = Toast.LENGTH_LONG
-            toast.view = layout
-            toast.show()
-
-            flag = if (flag == 1) {
-                imageView.setImageResource(R.drawable.icon_home_grey)
-                0
+    inner class CheckBoxListener : CompoundButton.OnCheckedChangeListener {
+        override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+            if (isChecked) {
+                selected += buttonView?.text.toString() + ","
             } else {
-                imageView.setImageResource(R.drawable.icon_home_selected)
-                1
+                selected = selected.replace(buttonView?.text.toString() + ",", "")
+                selected.trim()
             }
         }
-
     }
+
+
 }

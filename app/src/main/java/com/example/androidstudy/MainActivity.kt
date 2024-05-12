5297efb0ecbad4b3adfedae5b94f06304eb85ddf
weixu <xuwei19850423@gmail.com>
2024-05-12 16:37:04 +1000

    ViewBinding

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.androidstudy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidstudy.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var info: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btConfirmation.setOnClickListener{
+            info = binding.etName.text.toString()
+            Snackbar.make(binding.conLayout, info, BaseTransientBottomBar.LENGTH_LONG).show()
+        }
     }
 }

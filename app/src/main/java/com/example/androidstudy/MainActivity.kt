e5778dffb94075fc4aa1075945874339a9207ff1
weixu <xuwei19850423@gmail.com>
2024-07-11 19:48:11 +1000

    recyclerView in MainActivity

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidstudy
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerView)
+
+
     }
 }
